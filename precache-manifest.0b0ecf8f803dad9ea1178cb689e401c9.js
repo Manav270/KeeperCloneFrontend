479c81f672a51ef6427f410d9e1be6fbd034001d
Manav270 <manavarora7006@gmail.com>
2022-08-16 20:05:15 +0530

    Updates

---
+++ precache-manifest.0b0ecf8f803dad9ea1178cb689e401c9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c58059261b2e10003be2e4129e6ada",
+    "url": "/KeeperCloneFrontend/index.html"
+  },
+  {
+    "revision": "045f033e43fed0055d91",
+    "url": "/KeeperCloneFrontend/static/js/2.97b96ad9.chunk.js"
+  },
+  {
+    "revision": "10d637abf92312d36a3f",
+    "url": "/KeeperCloneFrontend/static/js/main.2eb26afc.chunk.js"
+  },
+  {
+    "revision": "5f62d7a6eaa5c3b17c5e",
+    "url": "/KeeperCloneFrontend/static/js/runtime-main.35b5d926.js"
+  }
+]);
